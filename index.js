c9b6d2e4ddf6c6f332936f3e2389f420e3a7195c
Estudiantes UPB <estudiante.upb@bga.upb>
2025-09-25 18:41:06 -0500

    Json de los menus

---
+++ index.js
@@ -0,0 +1,37 @@
+const menu=[
+
+{
+  "menuProductos": [
+    {
+      "idProducto": 1,
+      "nombre": "Hamburguesa Clásica",
+      "precio": 12000.00,
+      "categoria": "hamburguesa"
+    },
+    {
+      "idProducto": 2,
+      "nombre": "Coca-Cola",
+      "precio": 3500.00,
+      "categoria": "bebida"
+    },
+    {
+      "idProducto": 3,
+      "nombre": "Brownie de Chocolate",
+      "precio": 5000.00,
+      "categoria": "postre"
+    },
+    {
+      "idProducto": 4,
+      "nombre": "Pizza Margarita",
+      "precio": 25000.00,
+      "categoria": "hamburguesa"
+    },
+    {
+      "idProducto": 5,
+      "nombre": "Jugo de Naranja",
+      "precio": 4000.00,
+      "categoria": "bebida"
+    }
+  ]
+}
+]
